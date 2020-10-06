712d26882fe4a29036c8ba2c203a5ab7e3c94b15
Juan Carlos Pleitez <jcpleitez@outlook.com>
2020-10-06 18:40:54 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,4 +4,5 @@ def using_push(colors_in_the_rainbow,next_color)
   colors_in_the_rainbow << next_color
 end
 
-def using_unshift(bouroughs_in_new_york,)
+def using_unshift(bouroughs_in_nyc, new_neighborhood)
+  
